d76cab5b812b49e4b73d0e50f5335118eb409380
David P. Bauer <dbauer1@udayton.edu>
2019-07-29 08:44:58 -0400

    Adding video to the main learnudayton page

--- index.php
+++ index.php
@@ -9,11 +9,21 @@ require "nav.php";
 $OUTPUT->pageTitle('Welcome to the Learn Training Platform');
 ?>
 <p class="lead">Explore self-paced lessons on a variety of topics and technologies supported by the Office of eLearning.</p>
+<div class="row">
+    <div class="col-sm-7">
+        <div class="videoWrapper">
+            <iframe allowfullscreen src="https://udayton.warpwire.com/w/pwcBAA/"></iframe>
+        </div>
+        <h4>A Message from Ryan Allen, Director of the Office of eLearning</h4>
+    </div>
+    <div class="col-sm-5">
 <?php if ( isset($_SESSION['id']) ) { ?>
     <h2>Welcome, <?= $_SESSION['displayname'] ?>!</h2>
 <?php } ?>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut culpa doloribus expedita iste iusto minus necessitatibus, nesciunt, nisi officiis quas quisquam temporibus veritatis! Dolores explicabo maiores maxime nisi voluptatum.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium blanditiis consectetur ea expedita fuga iure.</p>
-<p><a href="<?=addSession($CFG->apphome.'/lessons')?>" title="Get Started" class="btn btn-primary">Get Started <span class="fas fa-arrow-right" aria-hidden="true"></span></a></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut culpa doloribus expedita iste iusto minus necessitatibus, nesciunt, nisi officiis quas quisquam temporibus veritatis! Dolores explicabo maiores maxime nisi voluptatum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium blanditiis consectetur ea expedita fuga iure.</p>
+        <p><a href="<?=addSession($CFG->apphome.'/lessons')?>" title="Get Started" class="btn btn-primary">Get Started <span class="fas fa-arrow-right" aria-hidden="true"></span></a></p>
+    </div>
+</div>
 <?php
 require "foot.php";
--- top.php
+++ top.php
@@ -19,3 +19,23 @@ try {
 if ( $PDOX !== false ) LTIX::loginSecureCookie();
 
 $OUTPUT->header();
+?>
+<style type="text/css">
+    div.videoWrapper {
+        position: relative;
+        padding-bottom: 56.25%; /* 16:9 */
+        padding-top: 25px;
+        height: 0;
+        margin-bottom: 1em;
+    }
+    div.videoWrapper iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    img {
+        width: 100%;
+    }
+</style>
