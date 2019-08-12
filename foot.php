f55c7409d5a5eb1a20d8838d15f4bb34aff05920
David P. Bauer <dbauer1@udayton.edu>
2019-08-12 11:50:08 -0400

    Add padding to footer

--- foot.php
+++ foot.php
@@ -1,7 +1,7 @@
 <?php
 
 $foot = '
-<div class="footer">
+<div class="footer" style="padding-top:1rem;">
 <p class="small text-muted"><em>Copyright <span class="far fa-copyright" aria-hidden="true"></span> '.date("Y").' University of Dayton | Office of eLearning<br/>
 The Office of eLearning is committed to ensuring digital accessibility for people with disabilities. All media content on this site has been closed captioned and labeled with the proper html attributes.</em></p>
 </div>';
