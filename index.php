ca2db993329bce14b2b1d0d685e3d26cf7624505
David P. Bauer <dbauer1@udayton.edu>
2019-08-13 10:07:02 -0400

    Fix column push pull

--- index.php
+++ index.php
@@ -10,13 +10,7 @@ $OUTPUT->pageTitle('Welcome to the Learn Training Platform');
 ?>
 <p class="lead">Explore self-paced lessons on a variety of topics and technologies supported by the Office of eLearning.</p>
 <div class="row">
-    <div class="col-sm-5 col-sm-push-7">
-        <div class="videoWrapper">
-            <iframe allowfullscreen src="https://udayton.warpwire.com/w/fw4BAA/"></iframe>
-        </div>
-        <h4>A Message from Ryan Allen, Director of the Office of eLearning</h4>
-    </div>
-    <div class="col-sm-7 col-sm-pull-5">
+    <div class="col-sm-5 col-sm-pull-7">
 <?php if ( isset($_SESSION['id']) ) { ?>
         <h2 style="margin-top:0;">Welcome, <?= $_SESSION['displayname'] ?>!</h2>
         <p>You can choose to complete the entire training packages or individual lessons. The training will be delivered through videos, text, links, and hands-on experiences. At the end of each lesson you will have the opportunity to prove your mastery and test your knowledge through built-in assignments.</p>
@@ -27,6 +21,12 @@ $OUTPUT->pageTitle('Welcome to the Learn Training Platform');
     <?php } ?>
         <p>Click on the <strong>Get Started</strong> link below to see the list of training modules available to you.</p>
         <p><a href="<?=addSession($CFG->apphome.'/lessons')?>" title="Get Started" class="btn btn-primary">Get Started <span class="fas fa-arrow-right" aria-hidden="true"></span></a></p>
+    </div>
+    <div class="col-sm-7 col-sm-push-5">
+        <div class="videoWrapper">
+            <iframe allowfullscreen src="https://udayton.warpwire.com/w/fw4BAA/"></iframe>
+        </div>
+        <h4>A Message from Ryan Allen, Director of the Office of eLearning</h4>
     </div>
 </div>
 <?php
