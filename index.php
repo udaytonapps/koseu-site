8a8df328ac2fe5b3684964260ab000a48fb7188f
David P. Bauer <dbauer1@udayton.edu>
2019-08-13 08:38:43 -0400

    Remove top margin from welcome

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $OUTPUT->pageTitle('Welcome to the Learn Training Platform');
     </div>
     <div class="col-sm-5">
 <?php if ( isset($_SESSION['id']) ) { ?>
-        <h2>Welcome, <?= $_SESSION['displayname'] ?>!</h2>
+        <h2 style="margin-top:0;">Welcome, <?= $_SESSION['displayname'] ?>!</h2>
         <p>Click on the <strong>Get Started</strong> link below to see the list of training modules available to you.</p>
         <p>You can choose to complete the entire training packages or individual lessons. The training will be delivered through videos, text, links, and hands-on experiences. At the end of each lesson you will have the opportunity to prove your mastery and test your knowledge through built-in assignments.</p>
         <p>The best part is that you can earn badges along the way as your complete different training packages. The badges serve as a reward and an acknowledgement of your newly mastered skills and dedication to learning. You can use the <strong>Assignments</strong> and <strong>Badges</strong> links in the top bar to keep track of your progress.</p>
