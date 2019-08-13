f6409cc2e5df20c26bf177bb76a42bf75d517aac
David P. Bauer <dbauer1@udayton.edu>
2019-08-13 08:37:34 -0400

    Update main page wording

--- index.php
+++ index.php
@@ -19,11 +19,13 @@ $OUTPUT->pageTitle('Welcome to the Learn Training Platform');
     <div class="col-sm-5">
 <?php if ( isset($_SESSION['id']) ) { ?>
         <h2>Welcome, <?= $_SESSION['displayname'] ?>!</h2>
-        <p>Click on the 'Get Started' link below to see the list of training modules available to you.</p>
-        <p>The training will be delivered through videos, text, links, and hands-on experiences. You'll have the opportunites to test your knowledge at he end of each lesson with an assessment and you will be awarded badges along the way as an acknowledge of your newly mastered skills and as a reward for your time.</p>
+        <p>Click on the <strong>Get Started</strong> link below to see the list of training modules available to you.</p>
+        <p>You can choose to complete the entire training packages or individual lessons. The training will be delivered through videos, text, links, and hands-on experiences. At the end of each lesson you will have the opportunity to prove your mastery and test your knowledge through built-in assignments.</p>
+        <p>The best part is that you can earn badges along the way as your complete different training packages. The badges serve as a reward and an acknowledgement of your newly mastered skills and dedication to learning. You can use the <strong>Assignments</strong> and <strong>Badges</strong> links in the top bar to keep track of your progress.</p>
 <?php } else { ?>
-        <p>Click on the 'Get Started' link below to see the list of training modules available to you.</p>
-        <p>The training will be delivered through videos, text, links, and hands-on experiences. Once you're logged in (hint: upper right corner) you'll have the opportunites to test your knowledge at he end of each lesson with an assessment and you will be awarded badges along the way as an acknowledge of your newly mastered skills and as a reward for your time.</p>
+        <p>Click on the <strong>Get Started</strong> link below to see the list of training modules available to you.</p>
+        <p>You can choose to complete the entire training packages or individual lessons. The training will be delivered through videos, text, links, and hands-on experiences. At the end of each lesson you will have the opportunity to prove your mastery and test your knowledge through built-in assignments once you’ve logged in (hint: upper-right corner of this page).</p>
+        <p>The best part is that you can earn badges along the way as your complete different training packages. The badges serve as a reward and an acknowledgement of your newly mastered skills and dedication to learning. Once you've logged in, you can use the <strong>Assignments</strong> and <strong>Badges</strong> links in the top bar to keep track of your progress.</p>
     <?php } ?>
         <p><a href="<?=addSession($CFG->apphome.'/lessons')?>" title="Get Started" class="btn btn-primary">Get Started <span class="fas fa-arrow-right" aria-hidden="true"></span></a></p>
     </div>
